db85929731527eaf542d6c9e66a685c45c001605
Aditya <aditya.chaudhary1558@gmail.com>
2024-04-13 21:18:56 +0530

    feat: added the url shorten logic

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,6 +1,11 @@
 package routes
 
-import "time"
+import (
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 type request struct {
 	URL         string        `json:"url"`
@@ -14,4 +19,28 @@ type response struct {
 	Expiry          time.Duration `json:"expiry"`
 	XRateRemaining  int           `json:"rate_limit"`
 	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(body); err != nil {
+		log.Fatal(err)
+	}
+
+	// implement rate limit:
+
+	// check if the URL is actually a valid URL
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(&fiber.Map{"error": "Invalid URL"})
+	}
+
+	// check for domain error
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(&fiber.Map{"error": "Domain Error"})
+	}
+	// enforce https, SSL
+
+	body.URL = helpers.EnforceHTTP(body.URL)
 }
